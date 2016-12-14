f70cfab11be1eb576297bb178e59391f017f40e0
dns2316 <dns2316@gmail.com>
2016-12-14 21:05:53 +0200

    diferents changed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SkyLight from 'react-skylight';
 import product from './productList';
+import styleSL from './other/styleForPopup';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+//src='img/'{item.id}'/01.jpg'
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welco1me to React</h2>
         </div>
         <div className="items">
           {product.map((item,index) => (
             <div key={index} className="item">
-              <img src={item.image} alt={item.name} />
+              <img className="preImg" src={item.image} alt={item.name} onClick={() => this.refs.customDialog.show()} />
+              <SkyLight dialogStyles={styleSL.dialogStyles} hideOnOverlayClicked ref="customDialog" title={item.name}>
+                <img className="bigImg" src={item.bigImage} alt={item.name} />
+                <br/>
+                <span className="composed">{item.composed} </span>
+              </SkyLight>
               <span className="nameItem">{item.name}</span><br/>
               <div className="priceItem">
                 <span className="priceItemPrice">{item.price} {item.currency}</span><span className="priceItemCur">1 шт</span>
--- src/productList.js
+++ src/productList.js
@@ -1,66 +1,84 @@
 const products = [
   {
+    id: 1,
     name: 'goods 1',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/1/01.jpg',
+    bigImage: 'img/1/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 2,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/2/01.jpg',
+    bigImage: 'img/2/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 3,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/3/01.jpg',
+    bigImage: 'img/3/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 4,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/4/01.jpg',
+    bigImage: 'img/4/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 5,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/5/01.jpg',
+    bigImage: 'img/5/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 6,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/6/01.jpg',
+    bigImage: 'img/6/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 7,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/7/01.jpg',
+    bigImage: 'img/7/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 8,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/8/01.jpg',
+    bigImage: 'img/8/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   },
   {
+    id: 9,
     name: 'goods',
     price: 100,
     currency: 'USD',
-    image: 'img/01.jpg',
-    url: '#'
+    image: 'img/9/01.jpg',
+    bigImage: 'img/9/orig.jpg',
+    composed: 'Состав: 1, 2, 3.'
   }
 ]
 export default products;
